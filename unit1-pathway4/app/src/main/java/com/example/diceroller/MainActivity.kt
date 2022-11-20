0c04e1eb9b7ed5dd809b67272a3eba7a0af38957
Kosuke Tomita <s163404@gmail.com>
2022-11-20 23:23:49 +0900

    PATHWAY4/Set image to ImageView

--- unit1-pathway4/app/src/main/java/com/example/diceroller/MainActivity.kt
+++ unit1-pathway4/app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -3,7 +3,7 @@ package com.example.diceroller
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
-import android.widget.TextView
+import android.widget.ImageView
 
 /**
  * This activity allows the user to roll a dice and view result
@@ -27,8 +27,8 @@ class MainActivity : AppCompatActivity() {
         val diceRoll = dice.roll()
 
         // Update the screen with the dice roll
-        val resultTextView: TextView = findViewById(R.id.textView)
-        resultTextView.text = diceRoll.toString()
+        val diceImage: ImageView = findViewById(R.id.imageView)
+        diceImage.setImageResource(R.drawable.dice_2)
     }
 
 }
