15e15718eaa7406b595e1974be5ea67b43882eb5
Kosuke Tomita <s163404@gmail.com>
2022-11-20 23:28:05 +0900

    PATHWAY4/Set an image that matches the roll of the dice

--- unit1-pathway4/app/src/main/java/com/example/diceroller/MainActivity.kt
+++ unit1-pathway4/app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -28,7 +28,14 @@ class MainActivity : AppCompatActivity() {
 
         // Update the screen with the dice roll
         val diceImage: ImageView = findViewById(R.id.imageView)
-        diceImage.setImageResource(R.drawable.dice_2)
+        when(diceRoll) {
+            1 -> diceImage.setImageResource(R.drawable.dice_1)
+            2 -> diceImage.setImageResource(R.drawable.dice_1)
+            3 -> diceImage.setImageResource(R.drawable.dice_3)
+            4 -> diceImage.setImageResource(R.drawable.dice_4)
+            5 -> diceImage.setImageResource(R.drawable.dice_5)
+            6 -> diceImage.setImageResource(R.drawable.dice_6)
+        }
     }
 
 }
