efc7a0454f480d533ee26b0a3cb7c264ad99f8a2
Kosuke Tomita <s163404@gmail.com>
2022-12-03 14:48:19 +0900

    Unit2-PATHWAY1/ビューバインディングのセットアップ1
    - build.gradle の記述によりビューバインディングの有効化
    - アクティビティでバインディングオブジェクトの初期化

--- unit2-pathway1/app/src/main/java/com/example/tiptime/MainActivity.kt
+++ unit2-pathway1/app/src/main/java/com/example/tiptime/MainActivity.kt
@@ -2,10 +2,20 @@ package com.example.tiptime
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.example.tiptime.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    // バインディングオブジェクトのクラス内の最上位変数を宣言
+    // `lateinit` コードが変数を使用する前に初期化することを保証する（初期化しない場合アプリがクラッシュする）
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        // XMLレイアウトのViewsへのアクセスに使うbindingオブジェクトを初期化する
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        // アクティビティのコンテンツビューを設定する。
+        // `setContentView(R.layout.activity_main)`の代わりにアプリのビュー階層のルートbinding.rootを指定する。
+        setContentView(binding.root)
     }
 }
