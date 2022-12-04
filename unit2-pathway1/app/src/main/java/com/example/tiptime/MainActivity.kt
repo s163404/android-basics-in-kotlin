a7e60206531cd51049887c8ae649b523f080a71d
Kosuke Tomita <s163404@gmail.com>
2022-12-04 15:46:42 +0900

    Unit2-PATHWAY1/チップ計算できるようにした
    - Calculateボタンにクリックリスナーを登録
      - EditTextビューから受け取った値の取得
      - チップを計算
      - 計算結果をTextViewに表示する

--- unit2-pathway1/app/src/main/java/com/example/tiptime/MainActivity.kt
+++ unit2-pathway1/app/src/main/java/com/example/tiptime/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.tiptime
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.tiptime.databinding.ActivityMainBinding
+import java.text.NumberFormat
 
 class MainActivity : AppCompatActivity() {
     // バインディングオブジェクトのクラス内の最上位変数を宣言
@@ -17,5 +18,35 @@ class MainActivity : AppCompatActivity() {
         // アクティビティのコンテンツビューを設定する。
         // R.layout.activity_mainの代わりにアプリのビュー階層のルートbinding.rootを指定する。
         setContentView(binding.root)
+
+        // クリックリスナーを登録
+        binding.calculateButton.setOnClickListener{ calculateTip() }
+    }
+
+    // チップを計算し、計算結果を表示する
+    fun calculateTip() {
+        // 料金を取得する
+        val stringTextField = binding.costOfService.text.toString()
+        val cost = stringTextField.toDouble()
+
+        // チップの割合を取得する
+        val selectedId = binding.tipOptions.checkedRadioButtonId
+        val tipPercentage = when (selectedId) {
+            R.id.option_twenty_percent -> 0.20
+            R.id.option_eighteen_percent -> 0.18
+            else -> 0.15
+        }
+
+        // チップを計算
+        // 必要に応じて端数を切り上げる
+        var tip = tipPercentage * cost
+        val roundUp = binding.roundUpSwitch.isChecked
+        if (roundUp) {
+            tip = kotlin.math.ceil(tip)
+        }
+
+        // チップの書式を設定して表示する
+        val formattedTip = NumberFormat.getCurrencyInstance().format(tip)
+        binding.tipResult.text = getString(R.string.tip_amount, formattedTip)
     }
 }
