b358d0d94967e2f8a905750f61b474188be2a3a7
Kosuke Tomita <s163404@gmail.com>
2023-01-26 23:05:32 +0900

    Unit2-PATHWAY2/テストランナーの指定、テストのためのアクティビティの起動、テスト関数を定義

--- unit2-pathway1/app/src/androidTest/java/com/example/tiptime/CalculatorTests.kt
+++ unit2-pathway1/app/src/androidTest/java/com/example/tiptime/CalculatorTests.kt
@@ -1,4 +1,19 @@
 package com.example.tiptime
 
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
 class CalculatorTests {
+    @get:Rule()
+    // アクティビティを起動
+    val activity = ActivityScenarioRule(MainActivity::class.java)
+
+    @Test
+    fun calculate_20_percent_tip() {
+
+    }
 }
